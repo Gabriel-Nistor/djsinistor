451b090d4c55539f1190caa24abe1757b690223d
Gabriel Nistor <gabriel.nistor1991@gmail.com>
2026-07-31 19:32:14 +0100

    More info button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 window.addEventListener("load", () => {
     document.body.classList.add("fade-in");
 });
+
+// More info:
+const moreInfoButton = document.querySelector(".more-info-btn");
+const moreInfoContent = document.querySelector(".more-info-content");
+const hideInfoButton = document.querySelector(".hide-info-btn");
+
+moreInfoButton.addEventListener("click", () => {
+    moreInfoContent.hidden = false;
+    moreInfoButton.hidden = true;
+    moreInfoButton.setAttribute("aria-expanded", "true");
+});
+
+hideInfoButton.addEventListener("click", () => {
+    moreInfoContent.hidden = true;
+    moreInfoButton.hidden = false;
+    moreInfoButton.setAttribute("aria-expanded", "false");
+
+    moreInfoButton.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+    });
+});
